91873c69b7e2b0df31f2f8708f00cca1174eee97
Ayushi Agarwal <ayushii.agarwal20@gmail.com>
2025-12-17 15:43:31 +0530

    [Ayushi] google classroom questions

---
+++ core-java-practice/gcr-codebase/programming-elements/AreaOfCircle.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class AreaOfCircle {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the radius of the circle:");
+        double radius = scanner.nextDouble();
+        double area = Math.PI * radius * radius;
+        System.out.println("Area of circle with radius " + radius + " is: " + area);
+    }
+}
---
+++ core-java-practice/gcr-codebase/programming-elements/CelciusToFahrenheit.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class CelciusToFahrenheit {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the temperature in Celsius:");
+        double celsius = scanner.nextDouble();  
+        double fahrenheit = (celsius * 9/5) + 32;
+        System.out.println(celsius + "°C is equal to " + fahrenheit + "°F");
+    }
+}
