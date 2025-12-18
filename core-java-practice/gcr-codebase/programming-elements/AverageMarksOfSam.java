b8adacaf9f8645cad625f41b21f3e6de6b8c91b2
Ayushi Agarwal <ayushii.agarwal20@gmail.com>
2025-12-18 11:58:20 +0530

    [Ayushi] Average Marks of Sam

--- core-java-practice/gcr-codebase/programming-elements/AverageMarksOfSam.java
+++ core-java-practice/gcr-codebase/programming-elements/AverageMarksOfSam.java
@@ -2,11 +2,11 @@
 
 public class AverageMarksOfSam {
     public static void main(String[] args) {
-        int physicsMarks = 95;
         int chemistryMarks = 94;
         int mathematicsMarks = 96;
-
+        int physicsMarks = 95;
         int totalMarks = physicsMarks + chemistryMarks + mathematicsMarks;
+        
         double averagePercent = (totalMarks / 300.0) * 100;
 
         System.out.println("Sam's average percent mark in PCM is: " + averagePercent + "%");    
