93e3bcece737b59c5bdce94c1cd8aa97df56679c
Ayushi Agarwal <ayushii.agarwal20@gmail.com>
2025-12-18 11:50:17 +0530

    [Ayushi] Harry's Age

--- core-java-practice/gcr-codebase/programming-elements/AgeOfHarry.java
+++ core-java-practice/gcr-codebase/programming-elements/AgeOfHarry.java
@@ -3,13 +3,13 @@ import java.util.Scanner;
 public class AgeOfHarry {
     public static void main(String[] args) {
         int birthYear = 2000;
-        Scanner sc = new Scanner(System.in);
+        Scanner input = new Scanner(System.in);
         System.out.println("Enter the current Year:");
-        int currentYear = sc.nextInt();
+        int currentYear = input.nextInt();
         
         int harrysAge = currentYear - birthYear;
         System.out.println("Harry's age is: " + harrysAge);
-        
-        sc.close();
+
+        input.close();
     }
 }
