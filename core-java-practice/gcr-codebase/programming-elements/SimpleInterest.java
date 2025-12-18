cc9cd7536647600feb8ec3db3695e0475b322289
Ayushi Agarwal <ayushii.agarwal20@gmail.com>
2025-12-18 12:02:22 +0530

    [Ayushi] Simple Interest

--- core-java-practice/gcr-codebase/programming-elements/SimpleInterest.java
+++ core-java-practice/gcr-codebase/programming-elements/SimpleInterest.java
@@ -1,17 +1,17 @@
 import java.util.Scanner;
 public class SimpleInterest {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
         System.out.println("Enter the principal amount:");
-        double principal = scanner.nextDouble();
+        double principal = sc.nextDouble();
         System.out.println("Enter the annual interest rate:");
-        double rate = scanner.nextDouble();
+        double rate = sc.nextDouble();
         System.out.println("Enter the time in years:");
-        double time = scanner.nextDouble();
+        double time = sc.nextDouble();
 
         double simpleInterest = (principal * rate * time) / 100;
         System.out.println("Simple Interest is " + simpleInterest);
 
-        scanner.close();
+        sc.close();
     }
 }
