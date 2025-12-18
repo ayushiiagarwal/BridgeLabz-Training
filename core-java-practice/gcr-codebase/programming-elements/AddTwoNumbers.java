5dcb4ca73cbdc63b73f7f3abbe96b73478b304ca
Ayushi Agarwal <ayushii.agarwal20@gmail.com>
2025-12-18 11:47:43 +0530

    [Ayushi] Sum of two numbers

--- core-java-practice/gcr-codebase/programming-elements/AddTwoNumbers.java
+++ core-java-practice/gcr-codebase/programming-elements/AddTwoNumbers.java
@@ -1,8 +1,15 @@
+import java.util.Scanner;
 public class AddTwoNumbers {
     public static void main(String[] args) {
-        int a = 5;
-        int b = 10;
-        int sum = a + b;
-        System.out.println("Sum of " + a + " and " + b + " is: " + sum);
+        Scanner sc = new Scanner(System.in);
+        int num1, num2, sum;
+        System.out.println("Enter first number:"); 
+        num1 = sc.nextInt();
+        System.out.println("Enter second number:");     
+        num2 = sc.nextInt();
+        sum = num1 + num2;
+        System.out.println("Sum of " + num1 + " and " + num2 + " is: " + sum);
+
+        sc.close();
     }
 }
