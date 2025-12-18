26d72d0b5d348a2c864e57580ccaedb2c388006a
Ayushi Agarwal <ayushii.agarwal20@gmail.com>
2025-12-18 11:58:44 +0530

    [Ayushi] Average of three numbers

--- core-java-practice/gcr-codebase/programming-elements/AverageOfThreeNumbers.java
+++ core-java-practice/gcr-codebase/programming-elements/AverageOfThreeNumbers.java
@@ -1,16 +1,16 @@
 import java.util.Scanner;
 public class AverageOfThreeNumbers {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
         System.out.println("Enter the first number:");
-        int num1 = scanner.nextInt();
+        int num1 = sc.nextInt();
         System.out.println("Enter the second number:");
-        int num2 = scanner.nextInt();
+        int num2 = sc.nextInt();
         System.out.println("Enter the third number:");
-        int num3 = scanner.nextInt();
+        int num3 = sc.nextInt();
         double average = (num1 + num2 + num3) / 3.0;
         System.out.println("Average = " + average);
 
-        scanner.close();
+        sc.close();
     }
 }
