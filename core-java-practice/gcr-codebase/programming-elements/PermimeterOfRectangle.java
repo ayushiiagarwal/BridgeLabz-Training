dc74d30f15cac2f087513103d0b2fd3650d68164
Ayushi Agarwal <ayushii.agarwal20@gmail.com>
2025-12-18 12:01:34 +0530

    [Ayushi] Perimeter of Rectangle

--- core-java-practice/gcr-codebase/programming-elements/PermimeterOfRectangle.java
+++ core-java-practice/gcr-codebase/programming-elements/PermimeterOfRectangle.java
@@ -1,14 +1,14 @@
 import java.util.Scanner;
 public class PermimeterOfRectangle {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
         System.out.println("Enter the length of the rectangle:");
-        int length = scanner.nextInt();
+        int length = sc.nextInt();
         System.out.println("Enter the width of the rectangle:");
-        int width = scanner.nextInt();
+        int width = sc.nextInt();
         int perimeter = 2 * (length + width);
         System.out.println("Perimeter of rectangle is: " + perimeter);
 
-        scanner.close();
+        sc.close();
     }
 }
