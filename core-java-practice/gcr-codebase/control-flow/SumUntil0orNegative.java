aedb2f68ce89b93156d93bb1d6f97c586f778678
Ayushi Agarwal <ayushii.agarwal20@gmail.com>
2025-12-19 16:40:06 +0530

    [Ayushi] Add .Add the numbers until the user enters ) or Negative number

---
+++ core-java-practice/gcr-codebase/control-flow/SumUntil0orNegative.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class SumUntil0orNegative {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        double total = 0;
+
+        while (true) {
+            System.out.println("Enter a number: ");
+            double input = sc.nextDouble();
+            if (input <= 0) break;
+            total += input;
+        }
+        System.out.println("Total sum is: " + total);
+        sc.close();
+    }
+}
