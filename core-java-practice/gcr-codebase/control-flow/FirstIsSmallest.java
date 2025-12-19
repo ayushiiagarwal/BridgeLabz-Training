d40532ff9c40074e6f759dcc66490a92a366300e
Ayushi Agarwal <ayushii.agarwal20@gmail.com>
2025-12-19 15:28:17 +0530

    [Ayushi] If first is the smallest of three

---
+++ core-java-practice/gcr-codebase/control-flow/FirstIsSmallest.java
@@ -0,0 +1,26 @@
+// check if the first is smallest of the 3 numbers
+
+import java.util.Scanner;
+public class FirstIsSmallest {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the first number: ");
+        int num1 = sc.nextInt();
+
+        System.out.println("Enter the second number: ");
+        int num2 = sc.nextInt();
+
+        System.out.println("Enter the third number: ");
+        int num3 = sc.nextInt();
+
+        if(num1 < num2){
+            if(num1 < num3){
+                System.out.println("Is the first number smallest? Yes");
+            }
+        }
+        else{
+            System.out.println("Is the first number smallest? No");
+        }
+        sc.close();
+    }
+}
