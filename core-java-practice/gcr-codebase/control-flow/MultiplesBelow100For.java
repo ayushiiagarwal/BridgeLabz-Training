0f26b28f065270b2ff2e5039e9765ab8d74e11c1
Ayushi Agarwal <ayushii.agarwal20@gmail.com>
2025-12-19 22:08:50 +0530

    [Ayushi] Add .Find the multiples of a number below 100 using For loop

---
+++ core-java-practice/gcr-codebase/control-flow/MultiplesBelow100For.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class MultiplesBelow100For {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a number: ");
+        int num = sc.nextInt();
+
+        if (num > 0 && num < 100) {
+            for (int i = 100; i >= 1; i--) {
+                if (i % num == 0) {
+                    System.out.println(i);
+                }
+            }
+        } else {
+            System.out.println("Invalid input.");
+        }
+        sc.close();
+    }
+}
