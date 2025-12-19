c39c9731c073bd5be10edfcca2e23bfedabf6890
Ayushi Agarwal <ayushii.agarwal20@gmail.com>
2025-12-19 15:58:06 +0530

    [Ayushi] Add .Check if the person can vote

---
+++ core-java-practice/gcr-codebase/control-flow/EligibleToVote.java
@@ -0,0 +1,17 @@
+// check if a person can vote or not depending on their age.
+
+import java.util.Scanner;
+public class EligibleToVote {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the age: ");
+        int age = sc.nextInt();
+        
+        if(age >=18)
+            System.out.println("The person's age is "+ age + " and can vote.");
+        else
+            System.out.println("The person's age is "+ age + " and cannot vote.");
+  
+        sc.close();
+    }
+}
