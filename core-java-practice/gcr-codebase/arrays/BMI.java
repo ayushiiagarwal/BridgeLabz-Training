d9720485cd927ed3c3268e0d1ea065811c916582
Ayushi Agarwal <ayushii.agarwal20@gmail.com>
2025-12-20 21:31:37 +0530

    [Ayushi] Add .Calculate the BMI using 1D Array

---
+++ core-java-practice/gcr-codebase/arrays/BMI.java
@@ -0,0 +1,39 @@
+
+
+import java.util.*;
+public class BMI {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int number = sc.nextInt();
+
+        double[] weight = new double[number];
+        double[] height = new double[number];
+        double[] bmi = new double[number];
+        String[] status = new String[number];
+
+        for (int i = 0; i < number; i++) {
+            weight[i] = sc.nextDouble();
+            height[i] = sc.nextDouble();
+
+            bmi[i] = weight[i] / (height[i] * height[i]);
+
+            if (bmi[i] < 18.5) 
+                status[i] = "Underweight";
+            else if (bmi[i] < 25) 
+                status[i] = "Normal";
+            else if (bmi[i] < 30) 
+                status[i] = "Overweight";
+            else 
+                status[i] = "Obese";
+        }
+
+        for (int i = 0; i < number; i++) {
+            System.out.println(height[i]);
+            System.out.println(weight[i]);
+            System.out.println(bmi[i]);
+            System.out.println(status[i]);
+        }
+
+        sc.close();
+    }
+}
