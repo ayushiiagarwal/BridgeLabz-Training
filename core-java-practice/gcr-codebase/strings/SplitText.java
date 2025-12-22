209a77926f336c04494ac2b40fe0bda9326c9f8c
Ayushi Agarwal <ayushii.agarwal20@gmail.com>
2025-12-22 21:53:11 +0530

    [Ayushi] Add .program to split the text into words

--- core-java-practice/gcr-codebase/strings/SplitText.java
+++ core-java-practice/gcr-codebase/strings/SplitText.java
@@ -28,6 +28,7 @@ public class SplitText{
 
     public static boolean compare(String[] str1, String[] str2) {
         if (str1.length != str2.length) return false;
+        
         for (int i = 0; i < str1.length; i++) {
             if (!str1[i].equals(str2[i])) return false;
         }
