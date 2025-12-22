20065b0a9fcf27e8c9eb10802b272e556c19b0aa
Ayushi Agarwal <ayushii.agarwal20@gmail.com>
2025-12-22 21:42:00 +0530

    [Ayushi] Add .Program to demonstrate ArrayIndexOutOfBoundsException

---
+++ core-java-practice/gcr-codebase/strings/ArrayException.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class ArrayException {
+
+    public static void generateException(String[] names) {
+        System.out.println(names[names.length]); 
+    }
+
+    public static void handleException(String[] names) {
+        try {
+            System.out.println(names[names.length]); 
+        } catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println("Caught you!");
+        }
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String[] names = new String[3];
+        for (int i = 0; i < 3; i++) names[i] = sc.next();
+        try {
+            generateException(names);
+        } catch (Exception e) {
+            handleException(names);
+        }
+
+        sc.close();
+    }
+}
