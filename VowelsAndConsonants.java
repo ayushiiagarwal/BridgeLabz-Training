4cd29f622b0cc2f63a6b7c300d01ee0c46b2a351
Ayushi Agarwal <ayushii.agarwal20@gmail.com>
2025-12-26 15:22:00 +0530

    [Ayushi] Add .Program to count the vowels and consonants in a string

--- VowelsAndConsonants.java
+++ VowelsAndConsonants.java
@@ -9,7 +9,8 @@ public class VowelsAndConsonants{
         int vowels = 0;
         int consonants = 0;
         for(int i=0;i<=ch.length-1;i++){
-            if(ch[i] == 'a' || ch[i] == 'e' || ch[i] == 'i' || ch[i] =='o' || ch[i] == 'u' )
+            if(ch[i] == 'a' || ch[i] == 'e' || ch[i] == 'i' || ch[i] =='o' || ch[i] == 'u' ||
+                ch[i] == 'A' || ch[i] == 'E' || ch[i] == 'I' || ch[i] == 'O' || ch[i] == 'U')
                 vowels++;
 
             else
