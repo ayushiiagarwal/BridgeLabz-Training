2a163f83bc636cbc254a64e4da2226af5ffdc863
Ayushi Agarwal <ayushii.agarwal20@gmail.com>
2026-01-14 15:45:04 +0530

    [Ayushi] Add .Check if there exists a pair of elements whose sum is equal to the target

--- dsa-practice/gcr-codebase/hashMap-hashing-function/CheckPair.java
+++ dsa-practice/gcr-codebase/hashMap-hashing-function/CheckPair.java
@@ -1,18 +1,28 @@
-import java.util.*;
+import java.util.HashMap;
 
 public class CheckPair {
-    public static void main(String[] args) {
-        int target = 30;
-        int[] arr = {10, 20, 45, 48, 12};
 
-        Set<Integer> set = new HashSet<>();
-        for (int num : arr) {
-            if (set.contains(target - num)) {
-                System.out.println("Yes");
-                return;
+    public static boolean hasPair(int[] arr, int target) {
+        HashMap<Integer, Boolean> map = new HashMap<>();
+
+        for (int i = 0; i < arr.length; i++) {
+            int required = target - arr[i];
+            if (map.containsKey(required)) {
+                return true;
             }
-            set.add(num);
+            map.put(arr[i], true);
         }
-        System.out.println("No");
+        return false;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {8, 7, 2, 5, 3, 1};
+        int target = 10;
+
+        if (hasPair(arr, target)) {
+            System.out.println("Pair exists");
+        } else {
+            System.out.println("Pair does not exist");
+        }
     }
 }
