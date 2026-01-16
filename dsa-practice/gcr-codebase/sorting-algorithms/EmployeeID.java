c21ce84a4b7bdb844d2acbca8a8f2b5a445b7f64
Ayushi Agarwal <ayushii.agarwal20@gmail.com>
2026-01-16 11:06:51 +0530

    [Ayushi] Add .Insertion Sort implementation to sort an array of Employee IDs

---
+++ dsa-practice/gcr-codebase/sorting-algorithms/EmployeeID.java
@@ -0,0 +1,26 @@
+import java.util.Arrays;
+
+public class EmployeeID {
+
+    public static void sort(int[] id){
+        int n = id.length;
+
+        for(int i=0;i<n;i++){
+            int key = id[i];
+            int j = i-1;
+            while(j >= 0 && id[j] > key){
+                id[j+1] = id[j];
+                j--;
+            }
+            id[j+1] = key;
+        }
+    }
+    public static void main(String[] args) {
+        int[] id = {101, 105, 102, 103, 104, 106};
+
+        System.out.println("Initial Array: " + Arrays.toString(id));
+        sort(id);
+
+        System.out.println("Sorted Array: " + Arrays.toString(id));
+    }
+}
