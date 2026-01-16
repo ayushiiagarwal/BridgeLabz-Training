6ecb328af179565b2790bcec62de2e54900fc966
Ayushi Agarwal <ayushii.agarwal20@gmail.com>
2026-01-16 11:00:36 +0530

    [Ayushi] Add .Bubble Sort Implementation to sort the Student marks in ascending order

---
+++ dsa-practice/gcr-codebase/sorting-algorithms/StudentMarks.java
@@ -0,0 +1,26 @@
+import java.util.Arrays;
+
+public class StudentMarks{
+    
+    public static void sortMarks(int[] marks){
+        int n = marks.length;
+
+        for(int i=0;i<n-1;i++){
+            for(int j=0;j<n-i-1;j++){
+                if(marks[j] > marks[j+1]){
+                    int temp = marks[j];
+                    marks[j] = marks[j+1];
+                    marks[j+1] = temp;
+                }
+            }
+        }
+    }
+    
+    public static void main(String[] args) {
+        int[] marks = {90, 85, 78, 60, 30};
+
+        System.out.println("Initial Marks: " + Arrays.toString(marks));
+        sortMarks(marks);
+        System.out.println("Sorted Marks: " + Arrays.toString(marks));
+    }
+}
