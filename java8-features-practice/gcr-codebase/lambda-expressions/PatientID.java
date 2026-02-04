4fdd74a2e703f3c68da49dafa67d97f2d28c338d
Ayushi Agarwal <ayushii.agarwal20@gmail.com>
2026-02-04 15:27:02 +0530

    [Ayushi] Add .Print all patinet IDs from a list

---
+++ java8-features-practice/gcr-codebase/lambda-expressions/PatientID.java
@@ -0,0 +1,8 @@
+import java.util.List;
+
+public class PatientID {
+    public static void main(String[] args) {
+        List<Integer> ids = List.of(101, 102, 103, 104, 105);
+        ids.forEach(System.out::println);
+    }
+}
